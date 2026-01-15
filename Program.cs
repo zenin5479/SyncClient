25fc0b284c846832f54aa683bf1373cf853b204a
zenin5479 <zenin5479@gmail.com>
2026-01-15 09:35:03 +0300

    Update 2.1 Обновлен метод. Проведен рефакторинг

--- Program.cs
+++ Program.cs
@@ -95,18 +95,18 @@ namespace SyncClient
 
       private static string SendPost(string path, object data)
       {
-         using (var client = new WebClient())
+         using (WebClient client = new WebClient())
          {
             client.Headers[HttpRequestHeader.ContentType] = "application/json";
-            var json = JsonConvert.SerializeObject(data);
+            string json = JsonConvert.SerializeObject(data);
             try
             {
-               var response = client.UploadString(BaseUrl + path, "POST", json);
+               string response = client.UploadString(BaseUrl + path, "POST", json);
                return response;
             }
             catch (WebException webEx)
             {
-               using (var errorResponse = webEx.Response as HttpWebResponse)
+               using (HttpWebResponse errorResponse = (HttpWebResponse)webEx.Response)
                {
                   if (errorResponse != null)
                   {
